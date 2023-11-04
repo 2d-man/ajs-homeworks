c408a6229e94a39be14d3d76f15a6cb7714b7085
2d-man <v.belyakov1997@gmail.com>
2023-11-04 17:15:00 +0300

    init

---
+++ ci-template/src/js/symbols_generator.js
@@ -0,0 +1,38 @@
+const char = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 1,
+    attack: 40,
+    defence: 10,
+  };
+  const char2 = {
+    name: 'Лучник2',
+    type: 'Bowman',
+    health: 50,
+    level: 1,
+    attack: 40,
+    defence: 10,
+  };
+  
+  class Team {
+    constructor() {
+      this.arrTeams = [char, char2];
+      this.currentIndex = 0;
+    }
+  
+    * [Symbol.iterator]() {
+    for (let item of this.arrTeams) {
+      yield item;
+  }
+    }
+  }
+  
+  
+  const objTeam = new Team();
+  
+  const iterator = objTeam[Symbol.iterator]();
+  
+  console.log(iterator.next());
+  console.log(iterator.next());
+  console.log(iterator.next());
